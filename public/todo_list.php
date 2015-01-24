928df9bc567c862ca60658c85e0ab182ff1a0f94
Stephanie <sriera3@gmail.com>
2015-01-24 14:55:40 -0600

    Final commit with working txt file uploader

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,64 +1,113 @@
 <?php
 
+require_once('../inc/filestore.php');
 
-$stufftodo = openfile('data/list.txt');
+//This is where I instantiate
+class todoStore extends Filestore{
 
-function openfile($filename) {
-	$contentsarray = [];
-		if(filesize($filename) > 0) {
-			$handle = fopen($filename, 'r');
-			$contents = trim(fread($handle, filesize($filename)));
-			$contentsarray = explode("\n", $contents);
-			fclose($handle);
-		}
-	return $contentsarray;
+	public $filename = '';
+	// public $stufftodo = [];
+
+	function __construct($filename) {
+		parent::__construct(strtolower($filename));
+	}
 }
 
 
- function savefile($filename, $array) {
-    $handle = fopen($filename, 'w');
-        foreach ($array as $item) {
-            fwrite($handle, $item . PHP_EOL);
-        }
-    fclose($handle);
- }
+//Need to instantiate the Filestore here
+$upload = new Filestore('uploads/list.txt');
+$stufftodo = $upload->read();
+
+
+
+// Verify there were uploaded files and no errors
+if (count($_FILES) > 0 && $_FILES['file1']['error'] == UPLOAD_ERR_OK) {
+    // Set the destination directory for uploads
+    $uploadDir = '/vagrant/sites/planner.dev/public/uploads/';
+
+    // Grab the filename from the uploaded file by using basename
+    $filename = basename($_FILES['file1']['name']);
+
+    // Create the saved filename using the file's original name and our upload directory
+    $savedFilename = $uploadDir . $filename;
+
+    $uploadFile = 'uploads/' . $filename;
+
+    // Move the file from the temp location to our uploads directory
+    move_uploaded_file($_FILES['file1']['tmp_name'], $savedFilename);
+    
+    $upload2 = new Filestore($uploadFile);
+
+    $newListItems = $upload2->read();
+
+    $stufftodo = array_merge($stufftodo, $newListItems);
+
+    $upload->write($stufftodo);
+}
+
+if (isset($savedFilename)) {
+    // If we did, show a link to the uploaded file
+    echo "<p>You can download your file <a href='/uploads/{$filename}'>here</a>.</p>";
+}
+
 
 //need to check if it exists
 if(isset($_POST['todo'])) {
 	$stufftodo[] = $_POST['todo'];
-	savefile('data/list.txt', $stufftodo);
+	$upload->write($stufftodo);
 }
 
 //must undo with unset
 if(isset($_GET['remove'])) {
 	$id = $_GET['remove'];
 	unset($stufftodo[$id]);
-	$stufftodo = array_values($stufftodo);
-	savefile('data/list.txt', $stufftodo);
+	// $stufftodo = array_values($stufftodo);
+	$upload->write($stufftodo);
 }
 
 
+//THIS IS EXCERCISE 9.2.1
+
+// if(empty($stufftodo)) {
+// 	throw new Exception('Your file is empty, upload something else.');
+// } elseif (strlen($stufftodo) > 240) {
+// 	throw new Exception('Your file is too large, upload something else.');
+// }
+
+//THIS IS EXCERCISE 9.2.2
+// try {
+// 	if(empty($filename)) {
+// 	echo "Your file is empty, upload something else.";
+// 	} elseif (strlen($filename) > 240) {
+// 		throw new Exception('Your file is too large, upload something else.');
+// 	}
+// } catch (Exception $e) {
+// 	echo $e->getMessage();
+// }
+
+
 ?>
-
-
 <!DOCTYPE HTML>
 <head>
 	<title>TO DO LIST</title>
 	<center><h1>To Do List</h1></center>
+<link href="Bootstrap/css/bootstrap.min.css" rel="stylesheet">
 <link rel="stylesheet" type="text/css" href="/css/todocss.css">
 <link href='http://fonts.googleapis.com/css?family=Shadows+Into+Light' rel='stylesheet' type='text/css'>
+
 </head>
 
 <body>
 	<h2>Current To Do List:</h2>
 		<ul>
 			<?php
-				foreach($stufftodo as $key => $value) {
-					echo "<li>{$value} | <a href=\"/todo_list.php?remove={$key}\">X</a></li>";
+				if(!empty($stufftodo)) {
+					foreach($stufftodo as $key => $value) {
+						echo "<li>{$value} | <a href=\"/todo_list.php?remove={$key}\">X</a></li>";
+					}
 				}
 			?>
 		</ul>
-
 <br>
 
 <hr>
@@ -70,10 +119,29 @@ if(isset($_GET['remove'])) {
 	    <br>
 	        <input id="todo" name="todo" type="text" placeholder="Add your task">
 	    </p>
-	 	
-	 	<br>
-
 		<button type="submit" class="btn btn-default">Add Task</button>
 	</form>
+
+	<br>
+
+	 <form method="POST" enctype="multipart/form-data" action="/todo_list.php">
+        <p>
+            <label for="file1">File to upload: </label>
+            <input type="file" id="file1" name="file1">
+        </p>
+        <p>
+            <input type="submit" value="Upload">
+        </p>
+    </form>
+    <!-- For jquery -->
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css">
+
+<!-- Optional theme -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap-theme.min.css">
+
+<!-- Latest compiled and minified JavaScript -->
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.2/js/bootstrap.min.js"></script>
 </body>
 </html>
