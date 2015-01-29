c7ddf8570df79c03d5d8dec5ce8e41e9d069b64c
Stephanie <sriera3@gmail.com>
2015-01-28 22:05:57 -0600

    Final commit

--- public/abdb_address.php
+++ public/abdb_address.php
@@ -2,25 +2,43 @@
 require_once('abdb_model.php');
 Class Address extends Model {
 	
-	public $nameAdd = 'Someone Temporary';
 	public $streetAdd = '';
 	public $aptAdd = '';
 	public $cityAdd = '';
 	public $stateAdd = '';
 	public $zipAdd = '';
-	public $fourAdd = '';
-	public $personId = 1;
+	public $personAdd = '';
+	public $addressRemove = '';
+
+
 	public function insert() {
-		$query = "INSERT INTO address (street, apt, city, state, zip, plus_four, person_id) VALUES (:street, :apt, :city, :state, :zip, :plus_four, :person_id)";
+		$query = "INSERT INTO address (street, apt, city, state, zip, person_id) VALUES (:street, :apt, :city, :state, :zip, :person_id)";
+		
 		$stmt = $this->dbc->prepare($query);
+
     	$stmt->bindValue(':street', $this->streetAdd, PDO::PARAM_STR);
     	$stmt->bindValue(':apt', $this->aptAdd, PDO::PARAM_STR);
     	$stmt->bindValue(':city', $this->cityAdd, PDO::PARAM_STR);
     	$stmt->bindValue(':state', $this->stateAdd, PDO::PARAM_STR);
     	$stmt->bindValue(':zip', $this->zipAdd, PDO::PARAM_STR);
-    	$stmt->bindValue(':plus_four', $this->fourAdd, PDO::PARAM_STR);
-    	$stmt->bindValue(':person_id', $this->personId, PDO::PARAM_INT);
-    	 $stmt->execute();
+    	$stmt->bindValue(':person_id', $this->personAdd, PDO::PARAM_INT);
+
+    	$stmt->execute();
+	}
+
+
+	public function delete() {
+
+	  $query = "DELETE FROM address where id = :id"; 
+	  
+	  $stmt = $this->dbc->prepare($query);
+	  $stmt->bindValue(':id', $this->addressRemove, PDO::PARAM_INT);
+
+	  $stmt->execute();
+
+	  	// var_dump($this->addressRemove);
+
+
 	}
 
 }
